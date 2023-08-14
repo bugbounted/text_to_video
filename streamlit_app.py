5dace376b4df83b18f4126f58601fddc392acec6
Hai <hai@Haiui-Mac-Studio.local>
2023-08-14 15:16:48 +0900

    00

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,33 +15,26 @@ import matplotlib.animation as animation
 from skimage.transform import resize
 
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+# Welcome to text to video!
 """
 
+pipe = DiffusionPipeline.from_pretrained("damo-vilab/text-to-video-ms-1.7b", torch_dtype=torch.float16, variant="fp16", device_map = 'auto')
+pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+pipe.enable_model_cpu_offload()
 
-with st.echo(code_location='below'):
-
-    pipe = DiffusionPipeline.from_pretrained("damo-vilab/text-to-video-ms-1.7b", torch_dtype=torch.float16, variant="fp16", device_map = 'auto')
-    pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
-    pipe.enable_model_cpu_offload()
+def main():
 
     prompt_text = st.text_input('Prompt', 'Write Prompt Here')
     num_inference_steps = st.number_input('num_inference_steps: default 25')
     num_frames = st.number_input('num_frames: default 20')
 
-    if st.button('Generate'):
+    if st.button('Generate and play'):
         video_frames = pipe(prompt_text, num_inference_steps=num_inference_steps,num_frames=num_frames).frames
         video_path = export_to_video(video_frames)
-
-    if st.button('Play'):
         st.video(video_path)
     
     if st.button('Download'):
         st.download_button(label="Download Video", data=video_path, file_name= f'{prompt_text}_video.mp4')
+
+if __name__ == "__main__":
+    main()
